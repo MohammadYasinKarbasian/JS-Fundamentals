ba2df850e19fd90dc62b192817c376ce8599ae0d
Mohammad Yasin Karbasian <82156191+MohammadYasinKarbasian@users.noreply.github.com>
2024-03-22 10:22:48 +0330

    Upload Code

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log('Hello');
+// let name = 'Yasin';
+// name = 'Mohammad';
+// console.log(name);
+
+// const name = 'Yasin';
+// console.log(name);
+
+// let height = 189;
+// let isTall = true
+// let lastName = undefined
+// let color = null
+
+// let student = {
+//     name: 'Yasin',
+//     height: 189,
+//     isTall: true
+// }
+
+// student.name = 'Mohammad';
+
+// student['name'] = 'Mohammad Yasin';
+
+// console.log(student)
+
+// let color = ['black', 'white'];
+// color.push('red');
+
+// console.log(color)
+
+
+// sayHi('Yasin', 'Karbasian')
+// function sayHi(firstName, lastName) {
+//     console.log('Hi, This is ' + firstName + ' ' + lastName)
+// }
+
+function multi(firstNumber, secondNumber){
+    return firstNumber * secondNumber
+}
+
+console.log(multi(5,4))
